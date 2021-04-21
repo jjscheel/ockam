57fb6aa233804f2f806f179c8b0c97d9a47dca29
Daniil Fedotov <daniil@ockam.io>
2021-04-21 11:55:35 -0400

    WIP: kafka storage for stream

---
+++ implementations/elixir/ockam/ockam_kafka/lib/ockam_kafka.ex
@@ -0,0 +1,150 @@
+defmodule Ockam.Hub.Service.Stream.Storage.Kafka do
+  @moduledoc """
+    Kafka storage backend for ockam stream service
+  """
+  @behaviour Ockam.Hub.Service.Stream.Storage
+
+  require Logger
+
+  @type options() :: Keyword.t()
+  @type message() :: %{index: integer(), data: binary()}
+
+  @default_worker :ockam
+
+  ## Storage API
+  @spec init(String.t(), options()) :: :ok | {:error, any()}
+  def init(stream_name, options) do
+    request = create_topics_request(stream_name, options)
+    worker_name = worker_name(options)
+    no_error = topic_error_none(request)
+    create_kafka_worker(options)
+    case KafkaEx.create_topics([request], [worker_name: worker_name]) do
+      %KafkaEx.Protocol.CreateTopics.Response {
+        topic_errors: [^no_error]
+      } ->
+        {:ok, options}
+      other ->
+        ## TODO: parse the error
+        Logger.error("Create topic error #{inspect(other)}")
+        {:error, other}
+    end
+  end
+
+  @spec save(String.t(), binary(), options()) :: {{:ok, integer()} | {:error, any()}, options()}
+  def save(stream_name, message, options) do
+    request = produce_request(stream_name, message, options)
+    worker_name = worker_name(options)
+    result = case KafkaEx.produce(request, [worker_name: worker_name]) do
+      :ok -> {:ok, 0}
+      {:ok, index} -> {:ok, index}
+      {:error, err} -> {:error, err}
+      other ->
+        Logger.error("Unexpected result from produce: #{inspect(other)}")
+        {:error, {:save_response, other}}
+    end
+    {result, options}
+  end
+
+  @spec fetch(String.t(), integer(), integer(), options()) :: {{:ok, [message()]} | {:error, any()}, options()}
+  def fetch(stream_name, index, limit, options) do
+    topic = topic(stream_name, options)
+    partition = partition(stream_name, index, limit, options)
+    fetch_options = fetch_options(stream_name, index, limit, options)
+
+    {fetch_messages(limit, topic, partition, fetch_options), options}
+  end
+
+  defp fetch_messages(limit, topic, partition, fetch_options, previous \\ []) do
+    prev_count = Enum.count(previous)
+
+    case KafkaEx.fetch(topic, partition, fetch_options) do
+      :topic_not_found -> {:error, :topic_not_found}
+      [%KafkaEx.Protocol.Fetch.Response{topic: topic} = fetch_response] ->
+        messages = get_response_messages(fetch_response)
+        case Enum.count(messages) do
+          0 -> {:ok, previous}
+          num when num + prev_count >= limit ->
+            {:ok, previous ++ Enum.take(messages, limit)}
+          _ ->
+            last_index = last_index(messages)
+            fetch_options = fetch_options(fetch_options, last_index + 1)
+            fetch_messages(limit, topic, partition, fetch_options, previous ++ messages)
+        end
+      other ->
+        Logger.error("Unexpected fetch response #{inspect(other)}")
+        {:error, {:fetch_response, other}}
+    end
+  end
+
+  def get_response_messages(%KafkaEx.Protocol.Fetch.Response{partitions: partitions}) do
+    partitions
+    |> Enum.flat_map(
+      fn(partition) ->
+        partition
+        |> Map.get(:message_set, [])
+        |> Enum.map(fn(%{offset: offset, value: value}) -> %{index: offset, data: value} end)
+      end)
+    |> Enum.sort_by(fn(%{index: index}) -> index end)
+  end
+
+  def last_index(messages) do
+    messages
+    |> Enum.map(fn(%{index: index}) -> index end)
+    |> Enum.max()
+  end
+
+  def create_kafka_worker(options) do
+    worker_name = worker_name(options)
+    ## TODO: pass worker config
+    KafkaEx.create_worker(worker_name)
+  end
+
+  defp worker_name(options) do
+    Keyword.get(options, :worker_name, @default_worker)
+  end
+
+  defp create_topics_request(stream_name, options) do
+    topic = topic(stream_name, options)
+    %KafkaEx.Protocol.CreateTopics.TopicRequest{topic: topic}
+  end
+
+  defp topic_error_none(topic_request) do
+    %KafkaEx.Protocol.CreateTopics.TopicError{
+      topic_name: topic_request.topic,
+      error_code: :no_error
+    }
+  end
+
+  defp produce_request(stream_name, message, options) do
+    topic = topic(stream_name, options)
+    %KafkaEx.Protocol.Produce.Request{
+      topic: topic,
+      partition: 0,
+      required_acks: 1,
+      messages: [%KafkaEx.Protocol.Produce.Message{value: message}]
+    }
+  end
+
+  defp topic(stream_name, _options) do
+    stream_name
+  end
+
+  defp partition(_stream_name, _index, _limit, _options) do
+    0
+  end
+
+  defp fetch_options(_stream_name, index, limit, options) do
+    [
+      worker_name: worker_name(options),
+      offset: index,
+      auto_commit: false,
+      ## Assume messages are 1Kb or so.
+      max_bytes: limit * 1024
+    ]
+  end
+
+  defp fetch_options(options, last_index) do
+    Keyword.put(options, :offset, last_index)
+  end
+
+end
---
+++ implementations/elixir/ockam/ockam_kafka/mix.exs
@@ -0,0 +1,31 @@
+defmodule OckamKafka.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ockam_kafka,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ockam, path: "../ockam"},
+      {:ockam_hub, path: "../ockam_hub"},
+      {:kafka_ex, "~> 0.12.1"},
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
